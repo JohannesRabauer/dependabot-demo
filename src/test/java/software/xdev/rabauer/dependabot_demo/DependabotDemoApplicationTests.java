a5e7b4c1588ae4d38721eaa3d192380aed3dcd85
JohannesRabauer <j.rabauer@xdev-software.de>
2025-04-10 14:01:59 +0200

    Init with 3.3.10

---
+++ src/test/java/software/xdev/rabauer/dependabot_demo/DependabotDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package software.xdev.rabauer.dependabot_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DependabotDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
